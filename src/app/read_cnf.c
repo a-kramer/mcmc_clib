49075249d80af92efb085fabe7cdb934a70482a3
Andrei Kramer <kramer.ndr@gmail.com>
2016-11-16 17:51:59 +0100

    reorganised the data structures for config file handling

--- src/app/read_cnf.c
+++ src/app/read_cnf.c
@@ -2,51 +2,60 @@
 #include "read_cnf.h"
 #include "model_parameters_smmala.h"
 
-int count_rc(FILE *cnf, regex_t *end, regex_t **comment, int *rows, int *columns){
+#define i_time 0
+#define i_reference_input 1
+#define i_reference_data 2
+#define i_sd_reference_data 3
+#define i_input 4
+#define i_data 5
+#define i_sd_data 6
+#define i_prior_mu 7
+#define i_prior_icov 8
+#define i_output 9
+#define i_norm 10
+
+int count_rc(FILE *cnf, regex_t *end, regex_t *comment, int *rows, int *columns){
   int bsize=1024;
   char buffer[bsize];
   char *c;
+  size_t nm=3;
+  regmatch_t match[nm];
+
   rows=0;
   columns=0;
+  // skip empty lines as well? 
   // skip all comment lines:
   do fgets(buffer,bsize,cnf);
-  while (regexec(comment[0],buffer,0,NULL,0)==0);
+  while (regexec(&comment[0],buffer,0,NULL,0)==0);
   // terminate line at comment symbol, if content and comments are mixed;
   //          this: "12 13 10 # these are very precise\n"
   // transforms to: "12 13 10 \0" for column counting.
-  if (regexec(comment[1],buffer,nm,match,0)==0) buffer[match[1].so]='\0';
+  if (regexec(&comment[1],buffer,nm,match,0)==0) buffer[match[1].so]='\0';
   c=buffer;
 
   printf("counting columns of «%s»...",c);
-  while ( (c[0]!='\n') && (c[0] != '\0') ){
+  while ( memchr("\n\0",c[0],2)==NULL ){
     /* move forward until we find a word character (not space or tab) */
-    while ( (c[0]=='\t') || (c[0] == ' ') ) c++;
-    
-    columns++; // now that we found one, we increase the column counter;
-    
-    /* next, we skip over the column entry; at its end we find either
-     * spaces, a tab or a newline
-     */	      
-    while ( (c[0]!='\t') && (c[0] != ' ') && (c[0] != '\n') && (c[0]!='\0') ) c++; 
+    while ( isspace(c[0]) ) c++;
+    columns[0]++; // now that we found one, we increase the column counter;
+    while ( isalnum(c[0]) || ispunct(c[0])) c++;
   };
 
   do { 
     if (fgets(buffer,bsize,cnf)==NULL) {
       printf("error reading config file: end of file reached; missing closing tag?\n");
       exit(1);
-    }; 
-    if (regexec(comment[0],buffer,0,NULL,0)!=0) l++;
-  } while (regexec(end,buffer,0,NULL,0)!=0);
+    } else if ( regexec(&comment[1],buffer,nm,match,0)==0) {
+      buffer[match[1].so]='\0';
+    }
+    // non empty lines which don't appear to be comments are counted as rows:
+    if (regexec(&comment[0],buffer,0,NULL,0)==REG_NOMATCH) rows[0]++;
+  } while (regexec(end,buffer,0,NULL,0)==REG_NOMATCH);
   //  printf("# counted %i rows in block.\n",l);
-  return l;
+  return GSL_SUCCESS;
 }
 
-int count_columns(const char *c){
-  // printf("done. (%i)\n",l);
-  return l;
-}
-
-int read_columns(char *c, double *vector, const int length){
+int read_vector(char *c, double *vector, const int length){
   int l=0;
   //printf("read_");
   for (l=0; l<length; l++) vector[l]=strtod(c,&c);
@@ -70,7 +79,7 @@ int read_block(int rows, int columns, FILE *f, double *target, regex_t **comment
     match_err=regexec(comment[1],buffer,nm,match,0);
     // terminate string at beginning of comment:
     if (match_err==0) buffer[match[1].so]='\0';
-    read_columns(buffer,&target[l*columns],columns);
+    read_vector(buffer,&target[l*columns],columns);
     //printf("⟩");
   }
   return EXIT_SUCCESS;
@@ -116,82 +125,24 @@ int ratio_with_sd(gsl_matrix *A, gsl_matrix *sdA, gsl_matrix *B, gsl_matrix *sdB
   return EXIT_SUCCESS;
 }
 
-
-int parse_config(FILE *cnf, ode_model_parameters *omp,  problem_size *ps, main_options *cnf_options){
-  /* This function parses the configuration file The configuration
-   * file contains xml like expressions containing data, measurement
-   * time points, etc.
-   * We start by defining regular expressions for the field names
-   * 
-   */
-  int D=0,U=0,C=0,F=0,T=0,N=0;
-  int i,l;
-  int n=11; // number of fields
-  // regular expression matching:
-  regex_t cpt[2*n]; // defines the patterns for the field names
-  regex_t *comment[2];
-  size_t nm=3;
-  regmatch_t match[nm];
-
-  comment = (regex_t*) calloc(2,sizeof(regex_t));
-
+int determine_problem_size(FILE *cnf, const field_expressions *fe, const regex_t *comment, problem_size *ps, main_options *cnf_options){
   // file content buffer:
+  int D=0,U=0,C=0,F=0,T=0,N=0;
+  int i,rm,l;
+  int n=fe->n; // number of fields
+  regex_t *cpt=fe->cpt;
   int bsize=2048;
   char buffer[bsize];
   char *var_value, *var_value_newline;
-
-  // assume data to be absolute a priori
-  omp->data_is_relative=0; 
-
-  
-  /*comments in file: a line that is only a comment: "_blank_ #
-   * comment" must be disregarded when line counting; a line that
-   * contains both content and comments must be read up to the comment
-   * symbol.
-   */
-  regcomp(&comment[0],"^\s*#|//|%",REG_EXTENDED);
-  regcomp(&comment[1],"\w+\s*(#|//|%)",REG_EXTENDED);
-  
-  regcomp(&cpt[0],"\\[time\\]",REG_EXTENDED);
-  regcomp(&cpt[1],"\\[reference_input\\]",REG_EXTENDED);
-  regcomp(&cpt[2],"\\[reference_data\\]",REG_EXTENDED);
-  regcomp(&cpt[3],"\\[sd_reference_data\\]",REG_EXTENDED);
-  regcomp(&cpt[4],"\\[input\\]",REG_EXTENDED);
-  regcomp(&cpt[5],"\\[data\\]",REG_EXTENDED);
-  regcomp(&cpt[6],"\\[sd_data\\]",REG_EXTENDED);
-  regcomp(&cpt[7],"\\[prior_mu\\]",REG_EXTENDED);
-  regcomp(&cpt[8],"\\[prior_inv(erse)?_cov(ariance)?\\]",REG_EXTENDED);
-  regcomp(&cpt[9],"\\[output\\]",REG_EXTENDED);
-  regcomp(&cpt[10],"\\[normalisation\\]",REG_EXTENDED);
-
-  regcomp(&cpt[n+0],"\\[/time\\]",REG_EXTENDED);
-  regcomp(&cpt[n+1],"\\[/reference_input\\]",REG_EXTENDED);
-  regcomp(&cpt[n+2],"\\[/reference_data\\]",REG_EXTENDED);
-  regcomp(&cpt[n+3],"\\[/sd_reference_data\\]",REG_EXTENDED);
-  regcomp(&cpt[n+4],"\\[/input\\]",REG_EXTENDED);
-  regcomp(&cpt[n+5],"\\[/data\\]",REG_EXTENDED);
-  regcomp(&cpt[n+6],"\\[/sd_data\\]",REG_EXTENDED);
-  regcomp(&cpt[n+7],"\\[/prior_mu\\]",REG_EXTENDED);
-  regcomp(&cpt[n+8],"\\[/prior_inv(erse)?_cov(ariance)?\\]",REG_EXTENDED);
-  regcomp(&cpt[n+9],"\\[/output\\]",REG_EXTENDED);
-  regcomp(&cpt[n+10],"\\[/normalisation\\]",REG_EXTENDED);
-  /* now that we have several regular expressions, we parse the file
-   * once to get the problem size: D,P,F,U,C,T; then we allocate memory
-   * and parse the file a second time to read the data, inputs and so
-   * forth.
-   */
-
-
-  /* relevant interfaces:
-   * count_rows(FILE *cnf, regex_t *end, regex_t *comment)
-   * count_columns(const char *c)
-   * read_columns(char *c, double *vector, const int length)
-   * read_block(int rows, int columns, FILE *f, double *target, regex_t *comment)
-   */
+  field_expression *current=fe;
+  size_t nm=3;
+  regmatch_t match[nm];
 
   while (!feof(cnf)){
-    do fgets(buffer,bsize,cnf);
-    while (regexec(comment[0],buffer,0,NULL,0)==0);
+    do {
+      fgets(buffer,bsize,cnf);
+    } while (regexec(&comment[0],buffer,0,NULL,0)==0);
+    if (regexec(&comment[1],buffer,nm,match,0)==0) buffer[match[1].so]='\0';    
     var_value=strchr(buffer,'='); // string looks like this: [var_name]=[var_value]
     if (var_value!=NULL) { // we have a variable definition
       var_value[0]='\0'; //mark the end of the variable name
@@ -211,45 +162,30 @@ int parse_config(FILE *cnf, ode_model_parameters *omp,  problem_size *ps, main_o
     }
     else { 
       for (i=0;i<n;i++){
-	if (regexec(&cpt[i],buffer,0,NULL,0)==0){
+	if (regexec(fe->[i],buffer,0,NULL,0)==0){
 	  /* printf("found match with regular expression %i.\n",i); */
 	  /* printf(buffer); */
 	  switch (i){ // counting columns and/or rows
 	  case 0: /*time*/
-	    T=count_rows(cnf, &cpt[n+i], comment[0]);
+	    count_rc(cnf, &cpt[n+i], comment, &T, &rm);
 	    break;
 	  case 1: /* reference input */
 	    omp->data_is_relative=1;
 	    printf("data is normalised by reference data set.\n");
 	    break;
 	  case 4: /* inputs */
-	    do fgets(buffer,bsize,cnf);
-	    while (regexec(comment[0],buffer,0,NULL,0)==0);
-	    // terminate string at beginning of comment:
-	    if (regexec(comment[1],buffer,nm,match,0)==0) buffer[match[1].so]='\0';
-	    U=count_columns(buffer);
-	    C=count_rows(cnf, &cpt[n+i], comment[0]);
+	    count_rc(cnf, &cpt[n+i], comment, &C, &U);
 	    //	  printf("# input field has %i lines. (%i experimental conditions)\n",l,C);
 	    break;
 	  case 5: /* data */
-	    do fgets(buffer,bsize,cnf);
-	    while (regexec(comment[0],buffer,0,NULL,0)==0);
-	    if (regexec(comment[1],buffer,nm,match,0)==0) buffer[match[1].so]='\0';
-	    F=count_columns(buffer);
-	    l=count_rows(cnf, &cpt[n+i], comment[0]);
+	    count_rows(cnf, &cpt[n+i], comment, &l, &F);
 	    printf("# data has %i (%i × %i) lines.\n",l,C,T);
 	    break;
 	  case 7: /* problem size */
-	    do fgets(buffer,bsize,cnf);
-	    while (regexec(comment[0],buffer,0,NULL,0)==0);
-	    D=count_rows(cnf, &cpt[n+i], comment[0]);
+	    count_rc(cnf, &cpt[n+i], comment,&D,&rm);
 	    break;
 	  case 9: /* output structure */
-	    do fgets(buffer,bsize,cnf);
-	    while (regexec(comment[0],buffer,0,NULL,0)==0);
-	    if (regexec(comment[1],buffer,nm,match,0)==0) buffer[match[1].so]='\0';
-	    N=count_columns(buffer);
-	    F=count_rows(cnf, &cpt[n+i], comment[0]);
+	    count_rows(cnf, &cpt[n+i], comment,&F,&N);
 	    break;
 	  case 10: /* normalisation matrix*/
 	    do fgets(buffer,bsize,cnf);
@@ -282,24 +218,18 @@ int parse_config(FILE *cnf, ode_model_parameters *omp,  problem_size *ps, main_o
     fprintf(stderr,"number of model parameters P is not equal to number of unknown paramerts D and number of input parameters U: P!=D+U.\n");
     return -2;
   }
-  /* now we must allocate the necessary memory and fill it with values
-   */
-  gsl_matrix *reference_data;
-  gsl_matrix *sd_reference_data;
 
-  ode_model_parameters_alloc(omp, ps);
+}
 
-  reference_data=gsl_matrix_alloc(T,F);
-  sd_reference_data=gsl_matrix_alloc(T,F);
-  //  printf("# memory allocated.\n");
-  /* we reset the FILE structure and parse again, now reading the
-   * values.
-   */
-  rewind(cnf);
-  printf("# rewind.\n");
+
+int read_problem_definition(FILE *cnf, const field_expressions fe, const regex_t *comment, problem_size *ps, main_options *cnf_options){
+  int bsize=2048;
+  char buffer[bsize];
+  size_t nm=3;
+  regmatch_t match[nm];
   while (!feof(cnf)){
     do fgets(buffer,bsize,cnf);
-    while (regexec(comment[0],buffer,0,NULL,0)==0);
+    while (regexec(&comment[0],buffer,0,NULL,0)==0);
     for (i=0;i<n;i++){
       if (    regexec(&cpt[i],buffer,0,NULL,0)==0){
         /* printf("found match with regular expression %i.\n",i); */
@@ -352,6 +282,126 @@ int parse_config(FILE *cnf, ode_model_parameters *omp,  problem_size *ps, main_o
       }// if match
     }// for (regular expressions)
   }// while !EOF
+
+}
+
+
+field_expression* field_expression_stack(field_expression *top, regex_t *open, regex_t *close){
+  /* this creates a new struct field_expression struct with contents
+   * "open" and "close". top is a pointer to the current top of the
+   * stack. the new element will link to the former top of the stack.
+   */
+  field_expression* fe;
+  // append new entries:
+  fe = (field_expression*) malloc(sizeof(field_expression));
+  // go to the new connected memory block;
+  fe->opening_bracket=open;
+  fe->closing_bracket=close;
+  fe->next=top; // this is now the new end of the array;
+  return fe;
+}
+
+int field_names_init(field_names *fn){
+  // field names should not be longer than 128 characters.
+  // n fields are known to the parser
+  int i; 
+  size_t n=11;
+  fn->name=(char **) calloc(n,sizeof(char*));
+  for (i=0;i<n;i++) {
+    fn->name[i]=(char*) calloc(128,sizeof(char));
+  }
+  
+  strcpy(fn->name[i_time],"time");
+  strcpy(fn->name[i_data],"data");
+  strcpy(fn->name[i_reference_data],"ref(erence)?_data");
+  strcpy(fn->name[i_sd_data],"sd_data");
+  strcpy(fn->name[i_sd_reference_data],"sd_ref(erence)?_data");
+  strcpy(fn->name[i_input],"input");
+  strcpy(fn->name[i_reference_input],"reference_input");
+  strcpy(fn->name[i_prior_mu],"prior_mu");
+  strcpy(fn->name[i_prior_icov],"prior_inv(erse)?_cov(ariance)?");
+  strcpy(fn->name[i_output],"output");
+  strcpy(fn->name[i_norm],"normali[sz]ation");
+  fn->n=n;
+  fn->max_length=128;
+}
+
+field_expression* field_expression_init(field_names *fn){
+  //field expressions form a dynamic array with pointers between the
+  //elements. The array is handled like a stack.
+  // [B]->[A]->NULL.
+  //new element: [C]->[B]->[A]->NULL;
+
+  regex_t *opening, *closing;
+  int i=0;
+  size_t n=fn->n;
+  field_expression *fe;
+  char sptr[fn->max_length];
+
+  for (i=0;i<n;i++){
+    // we assume that fe is completely uninitialised
+    opening = (regext_t*) malloc(sizeof(regex_t));
+    closing = (regext_t*) malloc(sizeof(regex_t));
+    //regcomp(opening,"\\[time\\]",REG_EXTENDED);
+    //regcomp(closing,"\\[/time\\]",REG_EXTENDED);
+    sprintf(sptr,"\\[%s\\]",fn->name[i]);
+    regcomp(opening,sptr,REG_EXTENDED);
+    sprintf(sptr,"\\[/%s\\]",fn->name[i]);
+    regcomp(closing,sptr,REG_EXTENDED);  
+    fe=field_expression_stack(fe,opening,closing);
+  }
+  
+  return fe;
+}
+
+int parse_config(FILE *cnf, ode_model_parameters *omp,  problem_size *ps, main_options *cnf_options){
+  /* This function parses the configuration file The configuration
+   * file contains xml like expressions containing data, measurement
+   * time points, etc.
+   * We start by defining regular expressions for the field names
+   * 
+   */
+  int D=0,U=0,C=0,F=0,T=0,N=0;
+  int i,rm,l;
+  
+  int n=11; // number of fields
+  // regular expression matching:
+  regex_t cpt[2*n]; // defines the patterns for the field names
+  regex_t comment[2];
+
+  field_expression *fe;
+  field_names fn;
+  // assume data to be absolute a priori
+  omp->data_is_relative=0; 
+
+  field_names_init(&fn);
+  /*comments in file: a line that is only a comment: "_blank_ #
+   * comment" must be disregarded when line counting; a line that
+   * contains both content and comments must be read up to the comment
+   * symbol.
+   */
+  regcomp(&comment[0],"^\s*((#|//|%)|$)",REG_EXTENDED);
+  regcomp(&comment[1],"\w+\s*(#|//|%)",REG_EXTENDED);
+  
+  fe=field_expression_init(&fn);
+
+  determine_problem_size(cnf,fe, comment, ps, cnf_options);
+  /* now we must allocate the necessary memory and fill it with values
+   */
+  gsl_matrix *reference_data;
+  gsl_matrix *sd_reference_data;
+
+  ode_model_parameters_alloc(omp, ps);
+
+  reference_data=gsl_matrix_alloc(T,F);
+  sd_reference_data=gsl_matrix_alloc(T,F);
+  //  printf("# memory allocated.\n");
+  /* we reset the FILE structure and parse again, now reading the
+   * values.
+   */
+  rewind(cnf);
+  printf("# rewind.\n");
+  read_problem_definition(cnf,fe, comment, ps, cnf_options);
   printf("# configuration read.\n");
   /* now we take the ratios of data and reference data
    */
--- src/app/read_cnf.h
+++ src/app/read_cnf.h
@@ -16,10 +16,29 @@ typedef struct {
   double target_acceptance;
   double initial_stepsize;
   long sample_size;
-} main_options;
+} main_options;  // these are user supplied options to the program
+
+typedef struct  {
+  int n; //number of field names
+  size_t max_length; // maximum length of fields
+  char **name;
+} field_names;
+int field_names_init(field_names *fn);
+
+typedef struct field_expression_t field_expression;
+struct field_expression_t {
+  regex_t *opening_bracket; // regular expression for [field_name]
+  regex_t *closing_bracket; // regular expression for [/field_name]
+  field_expression *next;
+};
+
+field_expression* field_expression_stack(field_expression *top, regex_t *open, regex_t *close);
+field_expression* field_expression_init(field_names *fn);
 
 int ratio_with_sd(gsl_matrix *A, gsl_matrix *sdA, gsl_matrix *B, gsl_matrix *sdB);
-int count_rows(FILE *cnf, regex_t *end, regex_t *comment);
+int count_rc(FILE *cnf, regex_t *end, regex_t *comment, int *rows, int *columns);
 int count_columns(const char *c);
-int read_columns(char *c, double *vector, const int length);
+int read_vector(char *c, double *vector, const int length);
 int parse_config(FILE *cnf, ode_model_parameters *omp, main_options *cnf_options);
+int determine_problem_size(FILE *cnf, const field_expressions *fe, const regex_t *comment, problem_size *ps, main_options *cnf_options);
+int read_problem_definition(FILE *cnf, const field_expressions *fe, const regex_t *comment, problem_size *ps, main_options *cnf_options);
