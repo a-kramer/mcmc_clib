3486a1b9181a3527a5ce9fdea762a5c8e95e6ead
Andrei Kramer <kramer.ndr@gmail.com>
2017-02-01 18:13:03 +0100

    absolute data bugfixes (when reading in data file)

--- run_pendlm_abs.sh
+++ run_pendlm_abs.sh
@@ -4,13 +4,14 @@
 # -b                        binary output 
 # -l ODEmodel11S26P4U.so    shared library of model
 # -c ODEmodel11S26P4U.cfg   configurations
-Model=pendulum
-SampleFile="sample/${Model}_`date +%Y-%m-%dT%Hh%Mm`.double"
+ModelCFG=pendlm_abs
+ModelSO=pendulum
+SampleFile="sample/${ModelCFG}_`date +%Y-%m-%dT%Hh%Mm`.double"
 SampleSize=$((1024**2))
 cat<<EOF
 $0
- redirecting standard output to $Model.out
- redirecting standard error  to $Model.err
+ redirecting standard output to $ModelCFG.out
+ redirecting standard error  to $ModelCFG.err
 
  output will be binary (${SampleFile})
  to load the sample in matlab or GNU Octave:
@@ -21,10 +22,10 @@ $0
           Sample = fread(fid,[NumOfParameters+1,SampleSize],'double');
                    fclose(fid);
 
- use «tailf $Model.out» to see progress (press «Ctrl-C» to exit tailf)
- redirecting standard output to $Model.out
-             standard error  to $Model.err
+ use «tailf $ModelCFG.out» to see progress (press «Ctrl-C» to exit tailf)
+ redirecting standard output to $ModelCFG.out
+             standard error  to $ModelCFG.err
  sampling now ...
 EOF
 
-bin/ode_smmala  -b -s ${SampleSize} -p -o $SampleFile -l ./$Model.so -c ./$Model.cfg 1> $Model.out 2> $Model.err
+bin/ode_smmala  -b -s ${SampleSize} -w $((SampleSize/8)) -o $SampleFile -l ./$ModelSO.so -c ./$ModelCFG.cfg 1> $ModelCFG.out 2> $ModelCFG.err
