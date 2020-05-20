9b46fc1c9cfc35894fffdbccb76a0b4606a9d54f
Andrei Kramer <kramer.ndr@gmail.com>
2020-05-20 15:56:36 +0200

    code for data import compiles and runs for the COVID19 model, produces correct hdf5 file

--- src/sb/data.c
+++ src/sb/data.c
@@ -2,7 +2,18 @@
 #include "re.h"
 #include <math.h>
 
-data_t* data_block_alloc(char *BaseName, int major, int minor, gsl_matrix *data, gsl_matrix *stdv, gsl_vector *input, gsl_vector *time, int likelihood_flag){
+data_t*
+data_block_alloc
+(char *BaseName,
+ int major,
+ int minor,
+ const gsl_matrix *data,
+ const gsl_matrix *stdv,
+ const gsl_vector *input,
+ const gsl_vector *time,
+ const gsl_vector *x0,
+ int likelihood_flag)
+{
   assert(time->size == data->size1);
   assert(data->size1 == stdv->size1 && data->size2 == stdv->size2);
   data_t *D=malloc(sizeof(data_t));
@@ -10,12 +21,23 @@ data_t* data_block_alloc(char *BaseName, int major, int minor, gsl_matrix *data,
   g_string_printf(Name,"E-%i.%i-%s",major,minor,BaseName);
   D->MajorIndex=major;
   D->MinorIndex=minor;
-  D->measurement=data;
-  D->noise=stdv;
-  D->input=input;
-  D->time=time;
   D->lflag=likelihood_flag;
   D->Name=Name;
+  
+  D->measurement=gsl_matrix_alloc(data->size1,data->size2);
+  gsl_matrix_memcpy(D->measurement,data);
+  
+  D->noise=gsl_matrix_alloc(stdv->size1,stdv->size2);
+  gsl_matrix_memcpy(D->noise,stdv);
+
+  D->input=gsl_vector_alloc(input->size);
+  gsl_vector_memcpy(D->input,input);
+
+  D->time=gsl_vector_alloc(time->size);
+  gsl_vector_memcpy(D->time,time);
+  
+  D->InitialValue=gsl_vector_alloc(x0->size);
+  gsl_vector_memcpy(D->InitialValue,x0);
   return D;
 }
 
@@ -25,35 +47,45 @@ unwrap_data
  const int *lflag, /* the measurement contributes to Likelihood (or not)*/
  const experiment_type *ExperimentType, /* Type: _Dose Response_ or _Time Series_ */
  const GPtrArray *ExperimentName,
- const GPtrArray *input_override, /* default input for each experiment */
+ const gsl_matrix *input_override, /* default input for each experiment */
  const gsl_vector *default_time, /* default measurement time */
  const sbtab_t *Input,
  const sbtab_t *Output,
+ const gsl_matrix *x0,
  map_t *IdxMap)
 {
   int i,j;
   int L;
   gsl_matrix *Y_DATA, *Y_STDV;
-  gsl_vector *u_j, *time;
+  gsl_vector *time;
   int nE=DataTable->len;
+  
   sbtab_t *DataTable_j;
   GPtrArray *Data=g_ptr_array_sized_new(nE);
   data_t *D;
-
   GPtrArray *uid=sbtab_find_column(Input,"!ID",NULL);
   GPtrArray *oid=sbtab_find_column(Output,"!ID",NULL);
   GPtrArray *ErrorName=sbtab_find_column(Output,"!ErrorName",NULL);
   gsl_matrix *input_block;
+  gsl_vector_view ThisExperimentsInput;
   gsl_vector_view input_row, time_view;
   gsl_matrix_view data_sub, stdv_sub;
-  int nU=uid->len;
+  gsl_vector_view x0_row;
+  int nU=input_override->size2;
+  assert(input_override);
+  printf("[%s] input_override (%i×%i) for %i Experiments and %i input parameters.\n",__func__,input_override->size1,input_override->size2,nE,table_length(Input)); fflush(stdout);
+  printf("[%s] initial values (%i×%i) for %i Experiments.\n",__func__,x0->size1,x0->size2,nE); fflush(stdout);
+  assert(input_override->size1 == nE);
+
+  
   char *E_j;
   assert(lflag);
   for (j=0;j<nE;j++) { // j is the major experiment index
     DataTable_j=g_ptr_array_index(DataTable,j);
     L=table_length(DataTable_j);
-    u_j=g_ptr_array_index(input_override,j);
+    ThisExperimentsInput=gsl_matrix_row(input_override,j);
     E_j=g_ptr_array_index(ExperimentName,j);
+    x0_row=gsl_matrix_row(x0,j);
     if (DataTable_j){
       Y_DATA=sbtab_columns_to_gsl_matrix(DataTable_j,oid,">",1.0);
       Y_STDV=sbtab_columns_to_gsl_matrix(DataTable_j,ErrorName,NULL,INFINITY);      
@@ -61,14 +93,14 @@ unwrap_data
       case time_series:
 	time=sbtab_column_to_gsl_vector(DataTable_j,"!Time");
 	map_index(IdxMap,j,0);
-	D=data_block_alloc(E_j,j,0,Y_DATA,Y_STDV,u_j,time,lflag[j]);
+	D=data_block_alloc(E_j,j,0,Y_DATA,Y_STDV,&(ThisExperimentsInput.vector),time,&(x0_row.vector),lflag[j]);
 	g_ptr_array_add(Data,D);
 	break;
       case dose_response:
 	input_block=gsl_matrix_alloc(L,nU);
 	for (i=0;i<L;i++) {
 	  input_row=gsl_matrix_row(input_block,i);
-	  gsl_vector_memcpy(&(input_row.vector),u_j);
+	  gsl_vector_memcpy(&(input_row.vector),&(ThisExperimentsInput.vector));
 	}
 	sbtab_update_gsl_matrix(input_block,DataTable_j,uid,">");
 	if (default_time) time_view=gsl_vector_subvector(default_time,j,1);
@@ -85,6 +117,7 @@ unwrap_data
 			     &(stdv_sub.matrix),
 			     &(input_row.vector),
 			     &(time_view.vector),
+			     &(x0_row.vector),
 			     lflag[j]);
 	  g_ptr_array_add(Data,D);
 	}
@@ -130,6 +163,8 @@ void write_data(gpointer data, gpointer user_data){
   H5LTset_attribute_int(h5data->group_id,H5_data_name,"major",&major, 1);
   H5LTset_attribute_int(h5data->group_id,H5_data_name,"minor",&minor, 1);
   H5LTset_attribute_double(h5data->group_id,H5_data_name,"time",D->time->data, D->time->size);
+  H5LTset_attribute_double(h5data->group_id,H5_data_name,"InitilValue",D->InitialValue->data, D->InitialValue->size);
+   
   H5LTset_attribute_double(h5data->group_id,H5_data_name,"input",D->input->data, D->input->size);
   /* write normalisation attributes */
   int rk, rt;
